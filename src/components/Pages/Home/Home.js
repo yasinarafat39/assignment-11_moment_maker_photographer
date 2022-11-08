fdddcfd25af14eace128b72e7ce6f749bb40bf44
yasinarafat39 <yasinarafat2557@gmail.com>
2022-11-08 19:58:17 +0600

    Banner section added in home page

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../../Banner/Banner';
 
 const Home = () => {
     return (
         <div className='max-w-screen-xl mx-auto'>
-            <h2>This is Home</h2>
+            <Banner></Banner>
         </div>
     );
 };

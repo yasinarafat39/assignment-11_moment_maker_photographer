6564118b6554d2ee6a47531a3937344df46b575e
yasinarafat39 <yasinarafat2557@gmail.com>
2022-11-09 20:47:16 +0600

    Implement Google login in Login  page

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,10 +1,11 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -21,8 +22,8 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
-    const LogInWithGoogle = (provider) => {
-        return signInWithPopup(auth, provider);
+    const LogInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
     }
 
     const updateUserProfile = (profile) => {

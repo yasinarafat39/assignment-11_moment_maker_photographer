96a9c9b33ec2f3341c7b1809d263b7a48540055c
yasinarafat39 <yasinarafat2557@gmail.com>
2022-11-09 14:31:12 +0600

    Photograpy is my deed section is added

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import CameraSection from './CameraSection/CameraSection';
 import Services from './Services/Services';
+import Talk from './Talk/Talk';
 
 const Home = () => {
     return (
         <div className='max-w-screen-xl mx-auto'>
             <Banner></Banner>
             <Services></Services>
+            <CameraSection></CameraSection>
+            <Talk></Talk>
         </div>
     );
 };
---
+++ src/components/Pages/Home/Talk/Talk.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const Talk = () => {
+    return (
+        <section className="bg-gray-100 text-gray-800 ">
+            <div className="container max-w-xl p-6 py-12 mt-24 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+                <div>
+                    <h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl text-gray-900">Photography is my Deed.</h2>
+                    <p className="max-w-3xl mx-auto mt-4 text-xl text-center text-gray-600">I love to shoot Photo.</p>
+                </div>
+                <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+                    <div>
+                        <h3 className="text-2xl font-bold tracking-tight sm:text-3xl text-gray-900">My job is to catch every second</h3>
+                        <p className="mt-3 text-lg text-gray-600">You're about to throw the most epic party for your friends and family? I will be your ride or die. My job is to catch every second, so you and your loved ones can just let go and live in the moment. You feel like we could vibe? Leave us a message and let’s get this party started.</p>
+                        <div className="mt-12 space-y-12">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div className="flex items-center justify-center w-12 h-12 rounded-md bg-green-600 text-gray-50">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg font-medium leading-6 text-gray-900">Your true best friend</h4>
+                                    <p className="mt-2 text-gray-600">There’s nothing more incredible than finding the one person you can’t live without. Your true best friend.</p>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div className="flex items-center justify-center w-12 h-12 rounded-md bg-green-600 text-gray-50">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg font-medium leading-6 text-gray-900">I want to capture the true versions of you two.</h4>
+                                    <p className="mt-2 text-gray-600">I want to photograph this connection. Whether we adventure somewhere beautiful together or explore the intimacy of your own home, I want to capture the true versions of you two.</p>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div className="flex items-center justify-center w-12 h-12 rounded-md bg-green-600 text-gray-50">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg font-medium leading-6 text-gray-900">Let’s create something real.</h4>
+                                    <p className="mt-2 text-gray-600">Don’t hold back. You only get this moment once. You be you, I’ll be me. Let’s create something real.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div aria-hidden="true" className="mt-10 lg:mt-0">
+                        <img src="https://source.unsplash.com/random/360x480" alt="" className="mx-auto rounded-lg shadow-lg bg-gray-500" />
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    );
+};
+
+export default Talk;

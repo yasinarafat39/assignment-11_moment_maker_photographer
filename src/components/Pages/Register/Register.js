616dd83ffcb308b07f0d1956b99bdf4ddb1ddafb
yasinarafat39 <yasinarafat2557@gmail.com>
2022-11-09 20:50:14 +0600

    Implement Google login method in Register page

--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -5,7 +5,7 @@ import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
 
-    const { createNewUser, updateUserProfile } = useContext(AuthContext);
+    const { createNewUser, updateUserProfile, LogInWithGoogle } = useContext(AuthContext);
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
@@ -35,6 +35,22 @@ const Register = () => {
             })
     }
 
+    // Google Login
+    const handleGoogleLogin = () => {
+        LogInWithGoogle()
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                navigate('/')
+                setError('');
+            })
+            .catch(error => {
+                setError(error)
+                console.log(error);
+            })
+    }
+
+    // Update User Profile
     const handleUpdateUserProfile = (name, photoURL) => {
         const profile = {
             displayName: name,
@@ -90,6 +106,14 @@ const Register = () => {
                         }
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Register</button>
+                        </div>
+                        <div className="flex justify-center space-x-4">
+                            <button onClick={handleGoogleLogin} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-gray-600 focus:ring-green-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
+                                    <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                                </svg>
+                                <p>Login with Google</p>
+                            </button>
                         </div>
                         <p className="text-xs text-center sm:px-6 text-gray-600">Already have an account?
                             <a rel="noopener noreferrer" href="/login" className="underline text-gray-800"> Log in</a>

441c7eaae4ef13a0939ea794004f1e50d77f18c1
yasinarafat39 <yasinarafat2557@gmail.com>
2022-11-17 12:04:53 +0600

    React photo view added in service banner image

--- src/components/Pages/Home/Services/SingleService.js
+++ src/components/Pages/Home/Services/SingleService.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { PhotoView } from 'react-photo-view';
+import { PhotoView, PhotoProvider } from 'react-photo-view';
 import { Link } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
+
+
+
 
 const SingleService = ({ service }) => {
     const { _id, img, description, price, serviceName } = service;
 
-
-
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
+
+            <PhotoProvider
+                speed={() => 800}
+                easing={(type) => (type === 2 ? 'cubic-bezier(0.36, 0, 0.66, -0.56)' : 'cubic-bezier(0.34, 1.56, 0.64, 1)')}
+            >
+                <PhotoView src={img} >
+                    <figure><img src={img} alt="Shoes" /></figure>
+                </PhotoView>
+            </PhotoProvider>
+
             <div className="card-body">
                 <h2 className="card-title">{serviceName}</h2>
                 <p>{description.slice(0, 100) + "..."}</p>
